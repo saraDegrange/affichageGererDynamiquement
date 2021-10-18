60fa0288d761446051799da68f4f09ba83d50275
Sara <degrangesara@gmail.com>
2021-10-18 11:26:47 +0200

    ajout du sousHeader et du paragraphe

--- script.js
+++ script.js
@@ -7,13 +7,17 @@ header.style.backgroundColor = '#e3b04b';
 header.style.color = 'white';
 header.style.textAlign = 'center';
 header.style.fontSize = '32px';
-header.style.padding = '5px'
+header.style.padding = '20px';
 document.body.append(header);
 
-// let sousHeader = document.createElement('div'); 
-// sousHeader.innerHTML = 
-// <div class = 'sousHeader'>
-//     <a href="index.html">Accueil</a>
-//     <a href="#">Une autre page</a>
-// </div>
-// document.body.append(sousHeader);
+let sousHeader = document.createElement('div'); 
+sousHeader.innerHTML = 
+"<div class = 'sousHeader'> <a href='index.html'>Accueil</a> / <a href=''>Une autre page</a> </div>";
+sousHeader.style.background = '#f1d6ab';
+sousHeader.style.padding = '10px';
+document.body.append(sousHeader);
+
+let paragraphe = document.createElement('p');
+paragraphe.textContent = "Ceci est un paragraphe crée avec Javascript ! ";
+paragraphe.style.padding = '10px';
+document.body.append(paragraphe);
